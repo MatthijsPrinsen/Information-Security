6b606002975fc03a64d5a51fce593d4b995051ca
MatthijsPrinsen <52781420+MatthijsPrinsen@users.noreply.github.com>
2026-02-24 19:32:39 +0100

    8/9 on 1, 2/9 on 2

--- Assignment 2/exercise2.py
+++ Assignment 2/exercise2.py
@@ -28,3 +28,42 @@ def get_input():
             data_bytes.append(char)
     #print(f"Key bytes: {len(key_bytes)}, Data bytes: {len(data_bytes)}", file=sys.stderr)
     return key_bytes, data_bytes
+
+
+def init_lookup_table(key):
+    N = len(key)
+    S = list(range(256))
+    K = [key[i % N] for i in range(256)]
+    
+    j = 0
+    for i in range(256):
+        j = (j + S[i] + K[i]) % 256
+        S[i], S[j] = S[j], S[i] 
+    return S
+
+
+def PRGA(S, data):
+    i = 0
+    j = 0
+    output = []
+    
+    # drop first 256 keystream bytes
+    for _ in range(256):
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        S[i], S[j] = S[j], S[i]
+    
+    # encrypt
+    for byte in data:
+        i = (i + 1) % 256
+        j = (j + S[i]) % 256
+        S[i], S[j] = S[j], S[i]
+        keystream_byte = S[(S[i] + S[j]) % 256]
+        output.append(byte ^ keystream_byte) 
+    return output
+
+if __name__ == "__main__":
+    key_bytes, data_bytes = get_input()
+    S = init_lookup_table(key_bytes)
+    c = PRGA(S, data_bytes)
+    sys.stdout.buffer.write(bytes(c))
