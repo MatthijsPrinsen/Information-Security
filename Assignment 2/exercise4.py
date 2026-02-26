cd186554f2b69c49a103115fd35013e6005056a9
MatthijsPrinsen <52781420+MatthijsPrinsen@users.noreply.github.com>
2026-02-26 14:38:28 +0100

    Passed all tcs for ex4

--- Assignment 2/exercise4.py
+++ Assignment 2/exercise4.py
@@ -8,8 +8,9 @@ Output: -1 if the private key is invalid, 0 if the public key is invalid and 1 i
     private key are valid
 """
 import sys
+from math import gcd
 
-def input():
+def get_input():
     input = sys.stdin.read()
     lines = input.strip().splitlines()
     first_line = lines[0]
@@ -19,6 +20,36 @@ def input():
     public = [int(x) for x in lines[2].split(' ')]
     return m, n, private, public
 
+
+def private_validation(key, m, n):
+    # check if superincreasing
+    total = 0
+    for i in range(len(key)):
+        if key[i] <= total:
+            return False
+        total += key[i]
+    if n <= total:
+        return False
+    if gcd(m, n) != 1:
+        return False
+    return True
+
+
+def public_validation(m, n, priv, pub):
+    generated_key = []
+    for i in range(len(priv)):
+        generated_key.append((priv[i]*m)%n)        
+    if generated_key == pub:
+      return True
+    return False  
+        
+        
 if __name__ == '__main__':
-    m, n, private, public = input()
-    print(m, n, private, public)
+    m, n, private, public = get_input()
+    if not private_validation(private, m, n):
+        print("-1")
+    elif not public_validation(m, n, private, public):
+        print("0")
+    else:
+        print("1")
+    
