775b648417c24dbb9177800771b1da0a21870b69
MatthijsPrinsen <52781420+MatthijsPrinsen@users.noreply.github.com>
2026-04-07 15:44:29 +0200

    laid out plan

---
+++ Assignment 5/HMAC.py
@@ -0,0 +1,24 @@
+'''
+Purpose: implement HMAC.
+Create a program that reads a key and a message, then applies the HMAC algorithm to it
+and shows the output. The hash function you should use is the Tiger hash you implemented
+in exercise 2. The input of the program is given in binary form: first the key, followed by the
+separator byte 0xFF, followed by the message. The key is never longer than 64 bytes. The
+output should also be in binary.
+Note: What isn’t stated very clearly in many places is that the key in the HMAC algorithm
+should be padded with consecutive 0x00 bytes until it has a length equal to 64 bytes. This is
+in fact necessary in order to do the XOR with the ipad and opad.
+If you are using Python to implement this program, due to Themis limitations the filename
+of the file containing the main function should be HMAC.py.
+'''
+
+# constants = opad, ipad
+#  inner hash = ipad, 
+#  outer hash = opad
+# Input = key, message
+#  The input of the program is given in binary form: first the key, followed by the separator 
+#  byte 0xFF, followed by the message
+# Apply HMAC
+#  import tiger hash
+#   at the key in the HMAC algorithm should be padded with consecutive 0x00 bytes until it has 
+#   a length equal to 64 byte 
